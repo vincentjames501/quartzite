281dfb68527a1368648435e86c02fa3a04a762c0
Vincent Pizzo <vincent.pizzo@singlewire.com>
2026-07-24 19:53:36 -0700

    2.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.vincentjames501/quartzite "2.3.0"
+(defproject io.github.vincentjames501/quartzite "2.3.1-SNAPSHOT"
   :description "Quarzite is a thin Clojure layer on top the Quartz Scheduler"
   :url "https://github.com/vincentjames501/quartzite"
   :scm {:name "git" :url "https://github.com/vincentjames501/quartzite"}
